ed01d5da3f551cfb85804d7c3677a858a5c9b56b
Shahboz79 <shukrulayevshahboz79@gmail.com>
2023-11-28 02:08:53 +0500

    continue

--- src/Main.java
+++ src/Main.java
@@ -2,45 +2,80 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        menu();
+    }
+    public static void menu(){
+        System.out.println("Click on one of the numbers below");
         Scanner scanner=new Scanner(System.in);
         System.out.println("1-Create");
         System.out.println("2-Active Task List");
         System.out.println("3-Finished Task List");
         System.out.println("4-Update (by id)");
         System.out.println("5-Delete (by id)");
-        System.out.println("6-Mark as done");
+        System.out.println("6-Mark as done (by id)");
         System.out.print("Select the desired number: ");
         int number=scanner.nextInt();
 
-        String size;
         switch (number) {
 
             case 1:
                 Scanner scanner1=new Scanner(System.in);
                 System.out.print("Enter title: ");
                 String title=scanner1.nextLine();
+
                 Scanner scanner2=new Scanner(System.in);
                 System.out.print("Enter content: ");
                 String content=scanner2.nextLine();
+
                 TaskService taskService=new TaskService();
-                taskService.create(new TaskDTO(title,content));
+                System.out.println(taskService.create(new TaskDTO(title, content)));
                 break;
 
-            case 42:
-                size = "Medium";
+            case 2:
+                System.out.println(new TaskService().getActiveTaskList());
                 break;
 
-            // match the value of week
-            case 44:
-                size = "Large";
+            case 3:
+                System.out.println(new TaskService().getFinishedTaskList());
                 break;
 
-            case 48:
-                size = "Extra Large";
-                break;
+            case 4:
+                Scanner upId= new Scanner(System.in);
+                System.out.print("Enter Task Id: ");
+                Integer id=upId.nextInt();
 
+                Scanner upTitle=new Scanner(System.in);
+                System.out.print("Enter title: ");
+                String uTitle=upTitle.nextLine();
+
+                Scanner upContent=new Scanner(System.in);
+                System.out.print("Enter content: ");
+                String uContent=upContent.nextLine();
+
+                TaskService utaskService=new TaskService();
+                System.out.println(utaskService.update(new TaskDTO(id, uTitle, uContent)));
+                break;
+            case 5:
+                Scanner dId= new Scanner(System.in);
+                System.out.print("Enter Delete Task Id: ");
+                Integer delId=dId.nextInt();
+
+                System.out.println(new TaskService().delete(delId));
+                break;
+            case 6:
+                Scanner fId= new Scanner(System.in);
+                System.out.print("Enter Task Id: ");
+                Integer finId=fId.nextInt();
+
+                String markAsDone = new TaskService().markAsDone(finId);
+                if (markAsDone==null){
+                    System.out.println("You have entered an invalid id. Start again");
+                    menu();
+                }else {
+                System.out.println(markAsDone);}
+                break;
             default:
-                size = "Unknown";
+                menu();
                 break;
 
         }
--- src/TaskService.java
+++ src/TaskService.java
@@ -1,17 +1,21 @@
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.List;
 
 public class TaskService {
 
     public String create(TaskDTO taskDTO){
         Connection con=DbUtill.dbConnection();
-        Statement statement = null;
         int effectedRows=0;
         try {
-            statement = con.createStatement();
-            String sql = "insert into tasks(title,content) values('%s','%s')";
-            sql = String.format(sql, taskDTO.getTitle(), taskDTO.getContent());
+            Statement statement = con.createStatement();
+            String sql = "insert into tasks(title,content,status,created_date)" +
+                    " values('"+ taskDTO.getTitle()+"','"+taskDTO.getContent()+"','"+TaskStatus.ACTIVE+"','"+LocalDateTime.now()+"')";
+//            sql = String.format(sql, taskDTO.getTitle(), taskDTO.getContent(),TaskStatus.ACTIVE, LocalDateTime.now());
             effectedRows = statement.executeUpdate(sql);
             con.close();
         } catch (SQLException e) {
@@ -23,4 +27,114 @@ public class TaskService {
 
     }
 
+    public List<Task> getActiveTaskList() {
+        Connection con=DbUtill.dbConnection();
+        ResultSet resultSet=null;
+        List<Task> tasks=new LinkedList<>();
+        try {
+            Statement statement = con.createStatement();
+            resultSet = statement.executeQuery("select id, title, content, status, " +
+                    "created_date from tasks where finished_date is null");
+            while (resultSet.next()){
+               Task task=new Task(resultSet.getInt("id"),
+                       resultSet.getString("title"),
+                       resultSet.getString("content"),
+                       TaskStatus.valueOf(resultSet.getString   ("status")),
+                       resultSet.getTimestamp("created_date").toLocalDateTime(),
+                       null);
+               tasks.add(task);
+
+            }
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        if (resultSet!=null){
+            return tasks;
+        }else return new LinkedList<>();
+
+    }
+    public List<Task> getFinishedTaskList() {
+        Connection con=DbUtill.dbConnection();
+        ResultSet resultSet=null;
+        List<Task> tasks=new LinkedList<>();
+        try {
+            Statement statement = con.createStatement();
+            resultSet = statement.executeQuery("select id, title, content, status, " +
+                    "created_date, finished_date from tasks where finished_date is not null");
+            while (resultSet.next()){
+                Task task=new Task(resultSet.getInt("id"),
+                        resultSet.getString("title"),
+                        resultSet.getString("content"),
+                        TaskStatus.valueOf(resultSet.getString   ("status")),
+                        resultSet.getTimestamp("created_date").toLocalDateTime(),
+                        null);
+                tasks.add(task);
+
+            }
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        if (resultSet!=null){
+            return tasks;
+        }else return new LinkedList<>();
+
+    }
+
+    public String update(TaskDTO taskDTO) {
+        Connection con=DbUtill.dbConnection();
+        int effectedRows=0;
+        try {
+            Statement statement = con.createStatement();
+            String sql = "update tasks set" +
+                    " title='"+ taskDTO.getTitle()+"', content='"+taskDTO.getContent()+"' where id= "+taskDTO.getId()+"";
+//            sql = String.format(sql, taskDTO.getTitle(), taskDTO.getContent(),TaskStatus.ACTIVE, LocalDateTime.now());
+            effectedRows = statement.executeUpdate(sql);
+            con.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        if (effectedRows!=0){
+            return new String("Successfully updated");
+        }else return new String("Error");
+    }
+
+    public String delete(Integer id) {
+        Connection con=DbUtill.dbConnection();
+        int effectedRows=0;
+        try {
+            Statement statement=con.createStatement();
+            String sql="delete from tasks where id="+id+"";
+            effectedRows=statement.executeUpdate(sql);
+            con.close();
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        if (effectedRows!=0){
+            return new String("Successfully deleted");
+        }else return new String("Error");
+    }
+
+    public String markAsDone(Integer finId) {
+        Connection con=DbUtill.dbConnection();
+        int effectedRows=0;
+        try {
+            Statement statement = con.createStatement();
+            String sql = "update tasks set" +
+                    " status='"+ TaskStatus.DONE+"', finished_date='"+LocalDateTime.now()+"' where id= "+finId+"";
+//            sql = String.format(sql, taskDTO.getTitle(), taskDTO.getContent(),TaskStatus.ACTIVE, LocalDateTime.now());
+            effectedRows = statement.executeUpdate(sql);
+            con.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        if (effectedRows!=0){
+            return new String("Marked as done");
+        }else{
+            return null;
+    }}
 }
